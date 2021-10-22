1d1d5ae0b53ee0bd6c4a0c12147e19289552d5a7
yuseth <yusethnataly@gmail.com>
2021-10-22 09:54:37 -0400

    empezando js

---
+++ cards.js
@@ -0,0 +1,12 @@
+export function createCards (num) {
+    const containerCard = document.createElement("div");
+    containerCard.className = `${num.themeCard}`;
+
+    const datesHeader = document.createElement("div");
+    datesHeader.className = "card__header card-red"
+    const dates = document.createElement("div");
+    dates.className = "card__header card-purple";
+    const dates = document.createElement("div");
+    dates.className = "card__header card-gray";
+   
+}
